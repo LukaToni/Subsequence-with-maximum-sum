1d9c4c5a41eed2596e481db76683a2fd39b9ff8b
Luka <luka.toni93@gmail.com>
2014-06-21 15:21:28 +0200

    Update HW-2.c

--- HW-2.c
+++ HW-2.c
@@ -1,81 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*
-int MIN_INT()
-{
-	int min = -1, minimum = 0;
-	while (min < 0)
-		{
-			minimum = min;
-			min--; 
-		}
-	return minimum;
-}
-*/
-int tabel[1000000]; // zaradi prostora inicaliziramo izven main 
+
+int tabel[1000000]; // inicialization out of main
 
 int main ()
 {
-	int length; // dolzina nase tabele
-	int s_start = 0; //zacetni index
-	int s_end = 0; //koncni index
-	int i; // za for zanko
-	int s_start_cur=0; //trenutni zacetni index
-	int s_end_cur = 0; //trenutni koncni index
-	int maxsum = 0; //MIN_INT(); // v maxsum vstavimo najnizjo vrednost int-a (-2^31)
-	int sum = 0; // zacetna vsota je 0
-	int length_sequence; //dolzina nasega zaporedja
+	int length; // length of zable
+	int s_start = 0; //start index
+	int s_end = 0; //end index
+	int i; 
+	int s_start_cur=0; //current start index
+	int s_end_cur = 0; //current end index
+	int maxsum = 0; //the biggest sum
+	int sum = 0; // sum
+	int length_sequence; //length of sequence
 
-	scanf ("%d", &length);	// pogledamo koliko elementov imamo
+	scanf ("%d", &length);	//read elements
+	
 	for (i = 0; i < length; ++i)
-		{
-			scanf ("%d", &tabel[i]); //v to tabelo vstavimo elemente
-		}
+			scanf ("%d", &tabel[i]); //inset elemetns in table
 
 	//iscemo najdaljse zaporedje 
 	// 10 -50 30 -43 29 -21 -31 -33 6 35 -38
 	// izpis
 	// 7 2 41
 
-  for ( i = 0; i < length; i++) // gremo cez use elemente
+	for ( i = 0; i < length; i++) // going through all elemets
 	{
-				///////////////////////////////////////////////////////////////////////////////////////////////////
-				//CE SO POZITIVNA STEVILA//////////////////////////////////////////////////////////////////////////
-				if (tabel[i] > 0 )  //ce je element tabele pozitiven
-				{
-						s_end_cur = i;	// spremenimo zadnji element zaporedja
-						sum = sum + tabel[i];	//sestejemo sum z vrednostjo tabele
+		//POSITIVE NUMBERS
+		if (tabel[i] >= 0 ) 
+		{
+			s_end_cur = i;	// change last element of sequence
+			sum = sum + tabel[i];	//sum
 					
-					if (sum > maxsum) // ce je sum vecji od nasega maxsum-a
-					{
-								maxsum = sum;  //maxsum dobi vrednost sum-a
-								s_start = s_start_cur; //zacetni index dobi vrednost trenutnega zacetnega indexa
-								s_end = s_end_cur; //koncni index dobi vrednost trenutnega koncnega indexa
-					}
-				}
-				///////////////////////////////////////////////////////////////////////////////////////////////////
-				//GLEDANJE ELEMENTOV NAPREJ ///////////////////////////////////////////////////////////////////////
-				if (tabel[i] < 0)   // ce (tabel[i]) je manjsa ali enka (0) 
-				{
-						if (sum + tabel[i] <= 0) // ce je sestevek sum in trenutna vrednost tabele manjsa ali enaka 0
-						{
-								s_start_cur = i + 1 ; // ker je negativna vrednost jo preskoci in gre na naslednjega
-								sum = 0; // sum gre na 0
-						
-						}
-						else 
-						{
-							sum = sum + tabel[i]; //drugace se v sum pristeje vrednost trenutne tabele
-							s_end_cur = i; //konec trenutnega zaporedja nastavimo na i
-						}
-				}
-				//////////////////////////////////////////////////////////////////////////////////////////////////
+			if (sum > maxsum) // if sum is bigger than our maxsum
+			{
+				maxsum = sum;  // maxsum get value of sum
+				s_start = s_start_cur; //start index get value of current start index
+				s_end = s_end_cur; //end index get value of current end index
+			}
+		}
+				
+		//NEGATIVE NUMBERS
+		if (tabel[i] < 0) 
+		{
+			if (sum + tabel[i] <= 0) // if sum and tabel[i] is lover or equal 0
+			{
+				s_start_cur = i + 1 ; // if it is negative pass it
+				sum = 0; // sum go to 0
+			}
+			else 
+			{
+				sum = sum + tabel[i]; // sum add value of tabele[i]
+				s_end_cur = i; //end of current sequence set to i
+			}
+		}
 	}
 
-	length_sequence = (s_end - s_start) + 1; //pristejemo enko ker se ena odsteje
+	length_sequence = (s_end - s_start) + 1; //add one because we one subtract
 
-	printf("%d %d %d", s_start, length_sequence, maxsum); // izpis (zacetek indexa, dolzina max_zaporedja, najvecje podzaporedje)
-	printf("\n");// skok v novo vrsto
+	printf("%d %d %d", s_start, length_sequence, maxsum); 
+	printf("\n");//new line
 	
 	return 0; 
 }
